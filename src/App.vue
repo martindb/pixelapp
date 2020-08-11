fe5f38303f072556b0839f0fd05bf3cbc8a4c055
martindb <martindb@users.noreply.github.com>
2020-08-11 19:06:35 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <Pixel msg="Hello Vue in CodeSandbox!"/>
+  </div>
+</template>
+
+<script>
+import Pixel from "./components/Pixel";
+
+export default {
+  name: "App",
+  components: {
+    Pixel
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
